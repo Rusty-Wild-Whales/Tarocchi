fc287aab8a8f9a425497aab6689aa57690b6ebf8
Aromia <f2940015504@gmail.com>
2025-09-12 21:32:05 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl text-purple-500">Hello world!</p>
+    </>
+  )
+}
+
+export default App
