fd247c6e0bd421915157b37f875f43be7bac5113
Jc-965 <jesse58300@gmail.com>
2025-09-13 04:41:48 -0400

    feat: added music file and music player

---
+++ src/components/MusicPlayer.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import backgroundMusic from "../assets/mystical song.wav";
+
+class MusicPlayer {
+  private static audio: HTMLAudioElement | null = null;
+
+  static init() {
+    if (!MusicPlayer.audio) {
+      MusicPlayer.audio = new Audio(backgroundMusic);
+      MusicPlayer.audio.loop = true;
+      MusicPlayer.audio.volume = 0.5;
+    }
+  }
+
+  static play() {
+    MusicPlayer.init();
+    MusicPlayer.audio?.play().catch((err) => {
+      console.warn("Autoplay blocked:", err);
+    });
+  }
+
+  static stop() {
+    MusicPlayer.audio?.pause();
+    if (MusicPlayer.audio) MusicPlayer.audio.currentTime = 0;
+  }
+}
+
+export default MusicPlayer;
